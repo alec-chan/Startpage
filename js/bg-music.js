d73d57d2faf4d0d86a439468356cb63c92300206
Alec <alecchan1@gmail.com>
2017-01-19 19:32:34 -0800

    ..

--- js/bg-music.js
+++ js/bg-music.js
@@ -1,8 +1,54 @@
 (function() {
   //read mute cookie initially when page is opened to set icon and sound
   var cookie = getCookie("mute");
-  var video = iframe.getElementById("yt");
   var icon = document.getElementById("icon");
+
+
+//load video
+
+// 2. This code loads the IFrame Player API code asynchronously.
+      var tag = document.createElement('script');
+
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+      // 3. This function creates an <iframe> (and YouTube player)
+      //    after the API code downloads.
+      var player;
+      function onYouTubeIframeAPIReady() {
+        player = new YT.Player('yt', {
+          height: '1',
+          width: '1',
+          videoId: 'VhXP8RC_v9I',
+          events: {
+            'onReady': onPlayerReady
+          },
+          playerVars: {
+            'autoplay': 1
+            'start': 60
+          }
+        });
+      }
+
+      // 4. The API will call this function when the video player is ready.
+      function onPlayerReady(event) {
+        event.target.playVideo();
+      }
+
+      // 5. The API calls this function when the player's state changes.
+      //    The function indicates that when playing a video (state=1),
+      //    the player should play for six seconds and then stop.
+      var done = false;
+
+      function stopVideo() {
+        player.stopVideo();
+      }
+
+//load video end
+
+
+
   if(cookie == "false"){
     //don't mute sound
     icon.class="fa fa-volume-up fa-lg";
@@ -10,7 +56,7 @@
   else if (cookie == "true") {
     //mute
     icon.class="fa fa-volume-off fa-lg";
-    video.mute();
+    player.mute();
   }
   else if(cookie == ""){
     setCookie("mute", "false", 100);
@@ -19,18 +65,15 @@
 
   toggle = document.getElementById("bg-toggle");
   toggle.onclick = function(){
-    var cookiee = getCookie("mute");
-    var videoo = iframe.getElementById("yt");
-    var iconn = document.getElementById("icon");
-    if(cookiee == "false"){
+    if(cookie == "false"){
       setCookie("mute", "true", 100);
-      iconn.class="fa fa-volume-off fa-lg";
-      videoo.mute();
+      icon.class="fa fa-volume-off fa-lg";
+      player.mute();
     }
     else if(cookiee=="true"){
       setCookie("mute", "false", 100);
-      iconn.class="fa fa-volume-up fa-lg";
-      videoo.unMute();
+      icon.class="fa fa-volume-up fa-lg";
+      player.unMute();
     }
   };
 
