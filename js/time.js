a41cc4fb2a8bc0793df6fbe15d56376644874f9a
Alec Chan <alecchan1@gmail.com>
2017-01-19 18:14:52 -0800

    Update time.js

--- js/time.js
+++ js/time.js
@@ -26,32 +26,32 @@
         dateSubtitle = document.getElementById("date");
 
     switch (month) {
-      case 0: month = 'January'
+      case 0: month = '一月'
         break;
-      case 1: month = 'February'
+      case 1: month = '二月'
         break;
-      case 2: month = 'March'
+      case 2: month = '三月'
         break;
-      case 3: month = 'April'
+      case 3: month = '四月'
         break;
-      case 4: month = 'May'
+      case 4: month = '五月'
         break;
-      case 5: month = 'June'
+      case 5: month = '六月'
         break;
-      case 6: month = 'July'
+      case 6: month = '七月'
         break;
-      case 7: month = 'August'
+      case 7: month = '八月'
         break;
-      case 8: month = 'September'
+      case 8: month = '九月'
         break;
-      case 9: month = 'October'
+      case 9: month = '十月'
         break;
-      case 10: month = 'November'
+      case 10: month = '十一月'
         break;
-      case 11: month = 'December'
+      case 11: month = '十二月'
         break;
     }
 
-    dateSubtitle.textContent = day + ' ' + month + ' ' + year;
+    dateSubtitle.textContent = year + '年 ' + month + ' ' + day+'日'
   }
 })()
